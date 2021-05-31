789d2241b24eea202f6215538513dd211b5880a1
HaoyangXu-1998 <50621255+HaoyangXu-1998@users.noreply.github.com>
2021-05-31 09:05:08 +0800

    add file of another homework

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <string>
+
+struct ListNode
+{
+	int id;
+	int password;
+	ListNode* next;
+
+	ListNode(const int id_, const int password_, ListNode* next_ = nullptr) {
+		id = id_;
+		password = password_;
+		next = next_;
+	}
+};
+
+struct SqList
+{
+	int* id;
+	int* passwords;
+	int size;
+	int current = 0;
+
+	SqList(int size_, int* passwords_) {
+		size = size_;
+		passwords = passwords_;
+		id = new int[size];
+		for (int i = 0; i < size; i++) {
+			id[i] = i + 1;
+		}
+	}
+};
+
+SqList* InitSqList(int n, int* passwords) {
+	SqList* p = new SqList(n, passwords);
+	return p;
+}
+
+
+ListNode* InitListNode(int n, int* passwords) {
+	ListNode* tail, * newNode = nullptr;
+	newNode = new ListNode(n, passwords[n - 1], newNode);
+	tail = newNode;
+	if (n > 1) {
+		for (int i = n - 2; i >= 0; i--) {
+			newNode = new ListNode(i + 1, passwords[i], newNode);
+		}
+	}
+	tail->next = newNode;
+	return newNode;
+}
+
+void printIds(ListNode* head, int m) {
+	using namespace std;
+	if (head->next == head) {
+		cout << (head->id) << endl;
+		delete head;
+		return;
+	}
+
+	ListNode* p, * q;
+	if (m == 1) {
+		p = head;
+		q = head;
+		while (q->next != p) q = q->next;
+	}
+	else {
+		p = head->next;
+		q = head;
+		m -= 1;
+		for (int i = 1; i < m; i++) {
+			q = q->next;
+			p = p->next;
+		}
+	}
+	cout << p->id << endl;
+	m = p->password;
+	q->next = p->next;
+	delete p;
+	printIds(q->next, m);
+}
+
+void printIds(SqList* p, int m) {
+	using namespace std;
+	if (p->size == 1) {
+		cout << p->id[p->size - 1] << endl;
+		delete p;
+		return;
+	}
+	int new_current = (p->current + m - 1) % p->size;
+
+	cout << p->id[new_current] << endl;
+
+	int new_m = (p->passwords)[new_current];
+	for (int i = new_current; i < p->size - 1; i++) {
+		p->id[i] = p->id[i + 1];
+		p->passwords[i] = p->passwords[i + 1];
+	}
+	p->size--;
+	p->current = new_current;
+	printIds(p, new_m);
+
+}
+
+
+int main(int argc, char** argv) {
+	using namespace std;
+	string method;
+	cout << "please input method: LinkedList or SqList?" << endl;
+	getline(cin, method);
+	while (method != "LinkedList" && method != "SqList") {
+		cin.clear();
+		cin.sync();
+		cout << "Wrong input! Please input again: " << endl;
+		getline(cin, method);
+	}
+	int n = 0;
+
+	cout << "please input n: " << endl;
+	while (!(cin >> n) || n <= 0) {
+		cin.clear();
+		cin.sync();
+		cout << "n must be a integer and greater than 0, please input again: " << endl;
+	}
+	cout << "please input n passwords: " << endl;
+	int* arr_password = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr_password[i];
+	}
+
+	int m = 0;
+	cout << "please input the first m: " << endl;
+	cin >> m;
+	cout << "The answer is:" << endl;
+	if (method == "LinkedList") {
+		ListNode* head = InitListNode(n, arr_password);
+		printIds(head, m);
+	}
+	else {
+		SqList* sqlist = InitSqList(n, arr_password);
+		printIds(sqlist, m);
+	}
+	return 0;
+}
