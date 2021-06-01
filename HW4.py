0d846b35bcbedebbc10ca77d77d58b21207bb430
HaoyangXu-1998 <50621255+HaoyangXu-1998@users.noreply.github.com>
2021-06-02 01:20:42 +0800

    Add files via upload

--- HW4.py
+++ HW4.py
@@ -88,76 +88,76 @@ def FAUREPTS(n0, npts, d, b):
         a = np.matmul(C, a) % b
         P[i, :] = np.matmul(b_pwrs, a)
     return P
-# %%
-def SOBOLMAT(c_vec, m_init, r):
-    q = len(c_vec) - 1
-    V = np.diag(np.ones(r))
-    if q > 0:
-        m_vec = np.concatenate((m_init, np.zeros(r - q))) 
-        m_state = m_init
-        for i in range(q, r):
-            m_next = m_state[0]
-            for j in range(1, q + 1):
-                m_next ^= (2**j * int(c_vec[j]) * int(m_state[q-j]))
-            m_vec[i] = m_next
-            m_state = np.concatenate((m_state[1:], [m_next]))
-        for j in range(r):
-            m_bin = B_ARY(m_vec[j], 2)
-            k = len(m_bin)
-            for i in range(k):
-                V[j-i, j] = m_bin[k-i-1]
-    return V
-# %%
-def GRAYCODE2(n):
-    tmp1 = B_ARY(n, 2)
-    tmp2 = B_ARY(int(np.floor(n/2)), 2)
-    if len(tmp2) < len(tmp1):
-        tmp2 = np.concatenate((tmp2, np.zeros(len(tmp1) - len(tmp2))))
-    return np.bitwise_xor(np.int32(tmp1), np.int32(tmp2))
-# %%
-def SOBOLPTS(n0, npts, d, p_vec, m_mat):
-    n_max = n0 + npts - 1
-    r_max = 1 + int(np.floor(np.log(n_max) / np.log(2)))
-    r = 1
-    P = np.zeros((npts, d))
-    y = np.zeros((r_max, d))
-    if n0 > 1:
-        r = 1 + int(np.floor(np.log(n0 - 1) / np.log(2)))
-    q_next = 2**r
-    a = B_ARY(n0 - 1, 2)
-    g = GRAYCODE2(n0 - 1)
-    V = np.zeros((r_max, r_max, d))
-    for i in range(d):
-        q = int(np.floor(np.log(p_vec[i]) / np.log(2)))
-        c_vec = B_ARY(p_vec[i], 2)
-        # V[:, :, i] = SOBOLMAT(c_vec, m_mat[i, :], r_max)
-        V[:, :, i] = SOBOLMAT(c_vec, m_mat[i], r_max)
-    b_pwrs = (1/2)**np.arange(1, r_max + 1)
-    for i in range(d):
-        for m in range(r):
-            for n in range(r):
-                y[m, i] += (V[m, n, i] * g[r-n-1]) % 2
-            # TODO: y[:, i] = (V[:, :, i] @ g[::-1]) % 2 might be ok?
-    for k in range(n0, n_max + 1):
-        if k == q_next:
-            r += 1
-            g = np.concatenate(([1], g))
-            l = 0 # 0-based
-            q_next *= 2
-        else:
-            for i in range(len(a) - 1, -1, -1):
-                if a[i] == 0:
-                    l = i
-                    break
-            g[l] = 1 - g[l]
-        a = NEXTB_ARY(a, 2)
-        for i in range(d):
-            for m in range(r):
-                # TODO: numpy accelaration
-                y[m, i] += V[m, r-l-1, i]
-                y[m, i] %= 2
-            P[k-n0, i] = np.sum(b_pwrs * y[:, i])
-    return P
+# # %%
+# def SOBOLMAT(c_vec, m_init, r):
+#     q = len(c_vec) - 1
+#     V = np.diag(np.ones(r))
+#     if q > 0:
+#         m_vec = np.concatenate((m_init, np.zeros(r - q))) 
+#         m_state = m_init
+#         for i in range(q, r):
+#             m_next = m_state[0]
+#             for j in range(1, q + 1):
+#                 m_next ^= (2**j * int(c_vec[j]) * int(m_state[q-j]))
+#             m_vec[i] = m_next
+#             m_state = np.concatenate((m_state[1:], [m_next]))
+#         for j in range(r):
+#             m_bin = B_ARY(m_vec[j], 2)
+#             k = len(m_bin)
+#             for i in range(k):
+#                 V[j-i, j] = m_bin[k-i-1]
+#     return V
+# # %%
+# def GRAYCODE2(n):
+#     tmp1 = B_ARY(n, 2)
+#     tmp2 = B_ARY(int(np.floor(n/2)), 2)
+#     if len(tmp2) < len(tmp1):
+#         tmp2 = np.concatenate((tmp2, np.zeros(len(tmp1) - len(tmp2))))
+#     return np.bitwise_xor(np.int32(tmp1), np.int32(tmp2))
+# # %%
+# def SOBOLPTS(n0, npts, d, p_vec, m_mat):
+#     n_max = n0 + npts - 1
+#     r_max = 1 + int(np.floor(np.log(n_max) / np.log(2)))
+#     r = 1
+#     P = np.zeros((npts, d))
+#     y = np.zeros((r_max, d))
+#     if n0 > 1:
+#         r = 1 + int(np.floor(np.log(n0 - 1) / np.log(2)))
+#     q_next = 2**r
+#     a = B_ARY(n0 - 1, 2)
+#     g = GRAYCODE2(n0 - 1)
+#     V = np.zeros((r_max, r_max, d))
+#     for i in range(d):
+#         q = int(np.floor(np.log(p_vec[i]) / np.log(2)))
+#         c_vec = B_ARY(p_vec[i], 2)
+#         # V[:, :, i] = SOBOLMAT(c_vec, m_mat[i, :], r_max)
+#         V[:, :, i] = SOBOLMAT(c_vec, m_mat[i], r_max)
+#     b_pwrs = (1/2)**np.arange(1, r_max + 1)
+#     for i in range(d):
+#         for m in range(r):
+#             for n in range(r):
+#                 y[m, i] += (V[m, n, i] * g[r-n-1]) % 2
+#             # TODO: y[:, i] = (V[:, :, i] @ g[::-1]) % 2 might be ok?
+#     for k in range(n0, n_max + 1):
+#         if k == q_next:
+#             r += 1
+#             g = np.concatenate(([1], g))
+#             l = 0 # 0-based
+#             q_next *= 2
+#         else:
+#             for i in range(len(a) - 1, -1, -1):
+#                 if a[i] == 0:
+#                     l = i
+#                     break
+#             g[l] = 1 - g[l]
+#         a = NEXTB_ARY(a, 2)
+#         for i in range(d):
+#             for m in range(r):
+#                 # TODO: numpy accelaration
+#                 y[m, i] += V[m, r-l-1, i]
+#                 y[m, i] %= 2
+#             P[k-n0, i] = np.sum(b_pwrs * y[:, i])
+#     return P
 # %%
 def goodLatticePoints(npts, d, a=8363):
     n = 32749
@@ -178,12 +178,12 @@ def L2_Star_Dis(P):
             tmp2[i, j] = np.prod(1 - np.max(P[:, [i, j]], axis=0))
     return (1/3)**d - (1/2)**(d-1) * np.mean(tmp1) + np.mean(tmp2)
 # %%
-T = 1
-d = 8
-r = 0.05
-sigma = 0.3
-S0 = 100
-K = 100
+# T = 1
+# d = 8
+# r = 0.05
+# sigma = 0.3
+# S0 = 100
+# K = 100
 # %%
 def trueAnswer(d = 8, T=1, r=0.05, sigma=0.3, S0=100, K=100):
     s2 = (d + 1) * (2*d + 1)/(6*d**2) * sigma**2
@@ -194,35 +194,35 @@ def trueAnswer(d = 8, T=1, r=0.05, sigma=0.3, S0=100, K=100):
     return C_BS
 # %%
 # Halton
-def HaltonPricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
-    res = np.zeros(npts)
-    dt = T/d
-    t = np.arange(1, d + 1) * dt
-    P = np.zeros((d, npts))
-    b_list = [2, 3, 5, 7, 11, 13, 17, 19]
-    for i in range(d):
-        P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
-    P = P.flatten().reshape((d, npts), order="F")
+# def HaltonPricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
+#     res = np.zeros(npts)
+#     dt = T/d
+#     t = np.arange(1, d + 1) * dt
+#     P = np.zeros((d, npts))
+#     b_list = [2, 3, 5, 7, 11, 13, 17, 19]
+#     for i in range(d):
+#         P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
+#     P = P.flatten().reshape((d, npts), order="F")
     
-    for i in range(npts):
-        p = P[:, i]
-        z = norm.ppf(p)
-        S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
-        res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
-    return res
-# %%
-def FaurePricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
-    res = np.zeros(npts)
-    dt = T/d
-    t = np.arange(1, d + 1) * dt
-    P = FAUREPTS(1, npts, d, 13)
-    P = P.flatten().reshape((d, npts), order="F")
-    for i in range(npts):
-        p = P[:, i]
-        z = norm.ppf(p)
-        S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
-        res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
-    return res
+#     for i in range(npts):
+#         p = P[:, i]
+#         z = norm.ppf(p)
+#         S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
+#         res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
+#     return res
+# # %%
+# def FaurePricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
+#     res = np.zeros(npts)
+#     dt = T/d
+#     t = np.arange(1, d + 1) * dt
+#     P = FAUREPTS(1, npts, d, 13)
+#     P = P.flatten().reshape((d, npts), order="F")
+#     for i in range(npts):
+#         p = P[:, i]
+#         z = norm.ppf(p)
+#         S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
+#         res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
+#     return res
 # # %%
 # def SobolPricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
 #     res = np.zeros(npts)
@@ -239,15 +239,15 @@ def FaurePricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
 #         res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
 #     return res
 # %%
-def MCPricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
-    res = np.zeros(npts)
-    dt = T/d
-    t = np.arange(1, d + 1) * dt
-    for i in range(npts):
-        z = norm.rvs(size=d)
-        S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
-        res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
-    return res
+# def MCPricing(npts, T=1, d=8, r=0.05, sigma=0.3, S0=100, K=100):
+#     res = np.zeros(npts)
+#     dt = T/d
+#     t = np.arange(1, d + 1) * dt
+#     for i in range(npts):
+#         z = norm.rvs(size=d)
+#         S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(z))
+#         res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
+#     return res
 # %%
 def getPrimeNumberList(d):
     p_list = [2]
@@ -362,6 +362,7 @@ def QMC_geo(method, sequence, npts, d, T=1, r=0.05, sigma=0.3, S0=100, K=100):
             P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
         Z = norm.ppf(P)
     elif sequence == "Korobov":
+        
         P = goodLatticePoints(npts, d)
         Z = norm.ppf(P)
     elif sequence == "Standard":
@@ -390,6 +391,202 @@ def QMC_geo(method, sequence, npts, d, T=1, r=0.05, sigma=0.3, S0=100, K=100):
             S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(Z[:, i]))
             res[i] = np.exp(-r * T) * max(np.prod(S**(1/d)) - K, 0)
     return res
+# %%
+# AV method
+def QMC_AV(weights, method, sequence, npts, d, T=1, r=0.05, sigma=0.3, S0=100, K=100):
+    res = np.zeros(npts)
+    dt = T/d
+    t = np.arange(1, d + 1) * dt
+    if sequence == "Faure":
+        b_list = getPrimeNumberList(d+1)
+        for i in range(len(b_list)):
+            if b_list[i] > d:
+                break
+        P = FAUREPTS(b_list[i+1]**4 - 1, npts, d+1, b_list[i+1])[1:, :]
+        Z1 = norm.ppf(P)
+        Z2 = norm.ppf(1 - P)
+    elif sequence == "Halton":
+        P = np.zeros((d, npts))
+        b_list = getPrimeNumberList(d)
+        for i in range(d):
+            P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
+        Z1 = norm.ppf(P)
+        Z2 = norm.ppf(1 - P)
+    elif sequence == "Korobov":
+        
+        P = goodLatticePoints(npts, d)
+        Z1 = norm.ppf(P)
+        Z2 = norm.ppf(1 - P)
+    elif sequence == "Standard":
+        P = np.random.random((d, npts))
+        Z1 = norm.ppf(P)
+        Z2 = norm.ppf(1 - P)
+
+    if method == "BB":
+        for i in range(npts):
+            B1 = generateBM(d, Z1[:, i], 0)
+            B2 = generateBM(d, Z2[:, i], 0)
+            S1 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B1[1:])
+            S2 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B2[1:])
+            res[i] = np.exp(-r * T) * (max(np.average(S1, weights=weights) - K, 0) + max(np.average(S2, weights=weights) - K, 0)) / 2
+
+    elif method == "PCA":
+        C = np.zeros((d, d))
+        for i in range(d):
+            for j in range(d):
+                C[i, j] = min(t[i], t[j])
+        w, V = np.linalg.eig(C)
+        A = np.matmul(V, np.diag(np.sqrt(w)))
+        B1 = B2 = np.zeros(d)
+        for i in range(npts):
+            B1 = np.matmul(A, Z1[:, i])
+            B2 = np.matmul(A, Z2[:, i])
+            S1 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B1)
+            S2 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B2)
+            res[i] = np.exp(-r * T) * (max(np.average(S1, weights=weights) - K, 0) + max(np.average(S2, weights=weights) - K, 0)) / 2
+    elif method == "Standard":
+        for i in range(npts):
+            S1 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(Z1[:, i]))
+            S2 = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(Z2[:, i]))
+            res[i] = np.exp(-r * T) * (max(np.average(S1, weights=weights) - K, 0) + max(np.average(S2, weights=weights) - K, 0)) / 2
+    return res
+# %%
+# TODO: CV method
+def QMC_CV(weights, method, sequence, npts, d=64, T=1, r=0.05, sigma=0.3, S0=100, K=100):
+    P_geo = trueAnswer(d)
+    res = np.zeros((npts, 2))
+    dt = T/d
+    t = np.arange(1, d + 1) * dt
+    if sequence == "Faure":
+        b_list = getPrimeNumberList(d+1)
+        for i in range(len(b_list)):
+            if b_list[i] > d:
+                break
+        P = FAUREPTS(b_list[i+1]**4 - 1, npts, d+1, b_list[i+1])[1:, :]
+        Z = norm.ppf(P)
+    elif sequence == "Halton":
+        P = np.zeros((d, npts))
+        b_list = getPrimeNumberList(d)
+        for i in range(d):
+            P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
+        Z = norm.ppf(P)
+    elif sequence == "Korobov":
+        P = goodLatticePoints(npts, d)
+        Z = norm.ppf(P)
+    elif sequence == "Standard":
+        Z = norm.rvs(size=(d, npts))
+
+    if method == "BB":
+        for i in range(npts):
+            B = generateBM(d, Z[:, i], 0)
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B[1:])
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-r * T) * max(np.prod(S**weights) - K, 0)
+
+    elif method == "PCA":
+        C = np.zeros((d, d))
+        for i in range(d):
+            for j in range(d):
+                C[i, j] = min(t[i], t[j])
+        w, V = np.linalg.eig(C)
+        A = np.matmul(V, np.diag(np.sqrt(w)))
+        B = np.zeros((d, npts))
+        for i in range(npts):
+            B[:, i] = np.matmul(A, Z[:, i])
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B[:, i])
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-r * T) * max(np.prod(S**weights) - K, 0)
+    elif method == "Standard":
+        for i in range(npts):
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(Z[:, i]))
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-r * T) * max(np.prod(S**weights) - K, 0)
+    b_star = np.sum((res[:, 1] - res[:, 1].mean()) * (res[:, 0] - res[:, 0].mean())) / np.sum((res[:, 1] - res[:, 1].mean())**2)
+    res = res[:, 0] - b_star * (res[:, 1] - P_geo)
+    return res
+# %%
+def QMC_IS(weights, method, sequence, npts, d=64, T=1, r=0.05, sigma=0.3, S0=100, K=100):
+    res = np.zeros((npts, 2))
+    dt = T/d
+    t = np.arange(1, d + 1) * dt
+    if sequence == "Faure":
+        b_list = getPrimeNumberList(d+1)
+        for i in range(len(b_list)):
+            if b_list[i] > d:
+                break
+        P = FAUREPTS(b_list[i+1]**4 - 1, npts, d+1, b_list[i+1])[1:, :]
+        Z = norm.ppf(P)
+    elif sequence == "Halton":
+        P = np.zeros((d, npts))
+        b_list = getPrimeNumberList(d)
+        for i in range(d):
+            P[i, :] = getVanDerCorputPoints(1, npts, b_list[i])
+        Z = norm.ppf(P)
+    elif sequence == "Korobov":
+        P = goodLatticePoints(npts, d)
+        Z = norm.ppf(P)
+
+
+    if method == "BB":
+        for i in range(npts):
+            B = generateBM(d, Z[:, i], 0)
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B[1:])
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-1/2 * np.sum(P[:, i]**2))
+
+    elif method == "PCA":
+        C = np.zeros((d, d))
+        for i in range(d):
+            for j in range(d):
+                C[i, j] = min(t[i], t[j])
+        w, V = np.linalg.eig(C)
+        A = np.matmul(V, np.diag(np.sqrt(w)))
+        B = np.zeros((d, npts))
+        for i in range(npts):
+            B[:, i] = np.matmul(A, Z[:, i])
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * B[:, i])
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-1/2 * np.sum(P[:, i]**2))
+    elif method == "Standard":
+        for i in range(npts):
+            S = S0 * np.exp((r - 0.5*sigma**2) * t + sigma * np.sqrt(dt) * np.cumsum(Z[:, i]))
+            res[i, 0] = np.exp(-r * T) * max(np.average(S, weights=weights) - K, 0)
+            res[i, 1] = np.exp(-1/2 * np.sum(P[:, i]**2))
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # %%
 # n_Faure_d=64
 d = 64
